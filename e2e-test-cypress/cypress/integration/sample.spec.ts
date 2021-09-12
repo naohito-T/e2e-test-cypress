e4b47e99c1c8f1893ae712858877e523d6093b87
naohito-T <naohito.tanaka0523@gmail.com>
2021-09-13 02:19:52 +0900

    cypress typescript 対応

---
+++ e2e-test-cypress/cypress/integration/sample.spec.ts
@@ -0,0 +1,8 @@
+describe('Check text', () => {
+  it('check text', () => {
+    cy.visit('http://localhost:3000');
+
+    // code タグ内にテキストが存在することを確認する
+    cy.contains('code', 'src/App.tsx');
+  });
+});
